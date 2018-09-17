d562d484f34776e2da4533d2ac2bb0e8b5b61815
MiracelVip <barth.silvo@web.de>
2018-09-17 18:17:52 +0200

    ping with Efficient
    for demonstation the ping loops is increased

--- 05_SendFile.ino
+++ 05_SendFile.ino
@@ -13,6 +13,7 @@ void SendFile(char *g_FileName, uint32_t FirstLine, uint32_t LinesToSend, uint16
   char line[64];  // char[48] enough for 7 sensors to read in buffer
   int n; //counters
  // int i; //counters
+ int lost_package = 0;
 
   radio.powerUp();
 
@@ -33,16 +34,22 @@ void SendFile(char *g_FileName, uint32_t FirstLine, uint32_t LinesToSend, uint16
   rdfile.seekSet(FirstLine*35); //TAKE care that 35 is smaller than and average srting volume in bytes but close enough to it
   
   while ((n = rdfile.fgets(line, sizeof(line))) > 0) { // START read file line by line untill endOfFile
-
-     ConvertStringToArray(line, g_SendMsg);
+    
+    ConvertStringToArray(line, g_SendMsg);
     g_SendMsg[0] = g_clipID*1000+task;
 
 
     Serial.print ("Line to send: "); for (byte iii = 0; iii<8; iii++){ Serial.print (g_SendMsg[iii]); Serial.print(", "); } Serial.println (" ");
     if (g_SendMsg[3] >= FirstLine){ // Send if line >= of desied fist line
 
-           if (!radio.writeFast(&g_SendMsg, sizeof(g_SendMsg))) { counter++; }
-         //  if (!radio.write(&g_SendMsg, sizeof(g_SendMsg))) { counter++; }
+           if (!radio.writeFast(&g_SendMsg, sizeof(g_SendMsg))) { 
+               counter++; }
+           //if (!radio.write(&g_SendMsg, sizeof(g_SendMsg))) { counter++; }
+           bool ok = radio.txStandBy(); 
+           if (ok == 0){lost_package = lost_package + 1; Serial.print("lost: "); Serial.println(lost_package);}
+           if (lost_package > 1000){break;}
+          
+
 
     }//END if
 
--- ping.ino
+++ ping.ino
@@ -9,22 +9,59 @@
       uint32_t RcvMsg[8] = {0, 0, 0, 0, 0, 0, 0, 0};
      // uint32_t timestamp = millis();
 
+      int numReadings = 100;
+      int readings[numReadings];      // the readings from the analog input
+      int readIndex = 0;              // the index of the current reading
+      float total = 0;                  // the running total
+      float average = 0;                // the average
+      bool ok;
+
+      for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
+
+
      // while (true){ ///     for (uint8_t i = 1; i < 255; i++ ) {
-       for (uint8_t i = 1; i < 25; i++ ) { 
-        Serial.print("ping. ");
+       for (uint8_t i = 1; i <= 1000000; i++ ) { 
+        //Serial.print("ping. ");
 
          SendMsg[1] = millis();
+         SendMsg[2] = average * 100;
          SendMsg[4] = analogRead(A7)*2*ARef/4096*1000; //Actual input voltage of Clip in mV. 4096 for 12-bits /1024 for 10-bits analog input
          
        ReadSensors(g_DataSensors);
        SendMsg[7] = g_DataSensors[4]*10000+g_DataSensors[5]; 
 
          // cout << endl << "Send Ping: " << SendMsg[0] << "," <<  SendMsg[1] << "," << SendMsg[2] << "," << SendMsg[3] << "," << SendMsg[4] << "," << SendMsg[5] << "," << SendMsg[6] << "," << SendMsg[7] << endl; 
-          radio.write(&SendMsg, sizeof(SendMsg));
+        ok = radio.write(&SendMsg, sizeof(SendMsg));
+
+
+         // subtract the last reading:
+
+          total = total - readings[readIndex];
+          // read from the sensor:
+          readings[readIndex] = ok;
+          // add the reading to the total:
+          total = total + readings[readIndex];
+          // advance to the next position in the array:
+          readIndex = readIndex + 1;
+        
+          // if we're at the end of the array...
+          if (readIndex >= numReadings) {
+            // ...wrap around to the beginning:
+            readIndex = 0;
+          }
+        
+          // calculate the average:
+          average = total / numReadings;
+          // send it to the computer as ASCII digits
+          Serial.println(average);
+
+        
           
           if ( radio.isAckPayloadAvailable() ) {
               radio.read(&RcvMsg, sizeof(RcvMsg));
-                      Serial.println("HAVE answer for ping. ");
+                      //Serial.println("HAVE answer for ping. ");
 
             
           //   cout <<"Recv: " << RcvMsg[0] << "," << RcvMsg[1] << "," << RcvMsg[2] << "," << RcvMsg[3] << "," << RcvMsg[4] << "," << RcvMsg[5] << "," << RcvMsg[6] << "," << RcvMsg[7] << endl; 
@@ -60,7 +97,7 @@
 
          }
          else {
-              Serial.println("NO answer for ping. ");
+             // Serial.println("NO answer for ping. ");
          // delay(5);
           
          } // end else
